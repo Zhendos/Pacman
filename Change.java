de957f9131e0aecef2700b1ebd881feb9ac5d511
Zhendos <youri_da_bomb@hotmail.com>
2015-06-03 10:58:26 +0200

    Create Change.java

---
+++ Change.java
@@ -0,0 +1,53 @@
+package org.bot.pacman.draw;
+
+public class Change {
+	/**
+	 * 
+	 * Getters
+	 * @return
+	 */
+	public int getOogX(){
+		return Draw.xOog;
+	}
+	
+	public int getOogY(){
+		return Draw.yOog;
+	}
+	
+	public int getPacmanX(){
+		return Draw.xPacman;
+	}
+	
+	public int getPacmanY(){
+		return Draw.yPacman;
+	}
+	
+	/**
+	 * 
+	 * setters
+	 * @param x
+	 * @return
+	 */
+	public int setOogX(int x){
+		x = getOogX();
+		return x;
+	}
+	
+	public int setOogY(int y){
+		y = getOogY();
+		return y;
+	}
+	
+	public int setPacmanX(int x){
+		x = getPacmanX();
+		return x;
+	}
+	
+	public int setPacmanY(int y){
+		y = getPacmanY();
+		return y;
+	}
+	
+	
+
+}
